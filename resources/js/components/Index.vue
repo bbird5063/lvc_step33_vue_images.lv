8658937104bce6a01fe8d438814ce7350a54f378
bbird5063 <bbird5063@gmail.com>
2024-12-15 06:08:28 +0200

    1. Установка dropzone js и прогрузка первых изображений

--- resources/js/components/Index.vue
+++ resources/js/components/Index.vue
@@ -1,13 +1,31 @@
 <template>
-	<div>
-		Index
+	<div class="w-25">
+		<!--class="btn - чтобы палец появился и +d-block а то размеры будут как у кнопки-->
+		<div ref="dropzone" class="btn d-block p-5 bg-dark text-center text-light">
+			Upload
+		</div>
 	</div>
 </template>
 
 <script>
-    export default {
-			name: 'Index'
-    }
+import Dropzone from 'dropzone';
+export default {
+	name: 'Index',
+
+	data() {
+		return {
+			dropzone: null,
+		}
+	},
+
+	mounted() {
+		this.dropzone = new Dropzone(this.$refs.dropzone, {
+			/** Опции из https://docs.dropzone.dev/configuration/basics/configuration-options */
+			url: 'qwqwqw', // обязательное свойство, указываем что угодно(пока это не имеет значения)
+			clickable: true, // true - по умолчанию
+		});
+	}
+}
 </script>
 
 <style scoped></style>
